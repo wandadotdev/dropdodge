f94faae72b4d8433d14e8ea2429a1834dac44f92
wandadotdev <derpwanda@gmail.com>
2024-10-31 09:40:11 -0500

    window setup

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import pygame
 import time
 import random
 
+WIDTH, HEIGHT = 1000, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Drop Dodge")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
